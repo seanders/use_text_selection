d5892e828d85fc48f9ed023e295ae92e86a9995f
Sean Miller <slicebo123@gmail.com>
2019-11-17 19:05:40 -0800

    Allow hook to scope values/callbacks to specific nodes
    * Clear selected text values/refs when user selects a different range

--- index.jsx
+++ index.jsx
@@ -19,41 +19,32 @@ function App() {
       <h2>Page One</h2>
       <Page pageNumber="1" />
       <Page pageNumber="2" />
-      {/* <Page pageNumber="3" /> */}
+      <Page pageNumber="3" />
       <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
 
 function Page({ pageNumber }) {
-  const [selectedText, selectionObject, selectionRects] = useTextSelection(400);
+  const [selectedText, selectionObject, selectionRect, ref] = useTextSelection(400);
 
-  console.group('selectionActions')
-
+  console.groupCollapsed("selectionActions");
+  console.log('Page: ', pageNumber)
   console.log('selectedText', selectedText);
   console.log("selectionObject", selectionObject);
-  console.log("selectionRects", selectionRects);
+  console.log("selectionRect", selectionRect);
   console.groupEnd('selectionActions')
 
 
   return (
     <div>
       <h4>Page Number: {pageNumber}</h4>
-      <p>
+      <p ref={ref}>
         single-origin coffee actually flexitarian XOXO distillery tousled cray
         food truck McSweeney's Thundercats banjo synth beard authentic viral
         semiotics plaid selfies gluten-free Marfa banh mi PBR retro 8-bit iPhone
         bespoke wayfarers fap bicycle rights Odd Future salvia Blue Bottle
-        biodiesel asymmetrical mustache blog brunch put a bird on it pork belly
-        pop-up vegan Wes Anderson literally sartorial locavore drinking vinegar
-        polaroid occupy direct trade yr tofu quinoa kale chips photo booth
-        Neutra sriracha 3 wolf moon cliche High Life fingerstache pickled
-        disrupt keffiyeh Kickstarter deep v swag sustainable Tonx DIY small
-        batch dreamcatcher ugh paleo street art shabby chic cardigan gastropub
-        Helvetica raw denim Carles Intelligentsia Shoreditch selvage tattooed
-        mixtape hoodie bitters artisan Truffaut mlkshk craft beer organic
-        aesthetic mumblecore chia pug Pinterest stumptown kogi meh tote bag
-        Schlitz cornhole roof party 90's ennui fashion axe fanny pack PBR&B
+        biodiesel asymmetrical mustache blog brunch put a bird on it pork
         Austin VHS before
       </p>
     </div>
--- useTextSelection.js
+++ useTextSelection.js
@@ -1,5 +1,11 @@
 import { useRef, useState, useEffect } from "react";
 
+// TODOs: certain methods trigger layout/paints like `getBoundingClientRect`
+// Investigate if theres some performance optimizations we can make here.
+// Look at how `FastDOM` handles this. Multiple invocations of this hook would trigger
+// `n` number of paints/layout. Calculating the rect of the selection needs to only
+// occur once. Need to memoize this value across instances of this hook.
+
 const debounce = (fn, debounceTime) => {
   // debounced function
   let timeoutId;
@@ -12,33 +18,58 @@ const debounce = (fn, debounceTime) => {
 
 const noop = () => null;
 
+function nodeContainsSelectedText(node, selection, partialContainment = true) {
+  return selection.containsNode(node, partialContainment);
+}
+
 const getClientRectsForSelection = selection => {
   // Netscape originally added support for multiple ranges. Currently, only Gecko-based browsers actually offer
   // support for multiple ranges. Practically speaking, there is only one range possible on document at one point at time.
   // See: https://www.w3.org/TR/selection-api/#h_note_15
-  const range = selection.getRangeAt(0)
-  const rect = range.getClientRects();
+  const range = selection.getRangeAt(0);
+  const rect = range.getBoundingClientRect();
   return rect;
-}
+};
 
-export default function useTextSelection(debounceMs, selectionCb = noop) {
+export default function useTextSelection(debounceMs = 0, selectionCb = noop) {
   const [selectionText, setSelectionText] = useState("");
-  const selectionObject = useRef(null)
+  const watchedNode = useRef(null);
+  const selectionObject = useRef(null);
   const selectionRects = useRef(null);
-  // const [selectionObject, setSelectionObject] = useState(null);
 
-  const debouncedSetSelection = debounce(selection => {
+  const _setSelection = selection => {
+    if (selection === null) {
+      selectionObject.current = null;
+      selectionRects.current = null;
+      setSelectionText(null);
+      return;
+    }
+
     // Saving the selection object in state was breaking after 2 renders...?
     selectionObject.current = selection;
     selectionRects.current = getClientRectsForSelection(selection);
     setSelectionText(selection.toString());
-  }, 400);
+  };
+
+  // If debounce is 0, assume this means the user wants synchronous execution
+  const setSelectedText =
+    debounceMs <= 0 ? _setSelection : debounce(_setSelection, debounceMs);
 
   function handleSelectionChange(event) {
     const selection = document.getSelection();
+    const nodeContainsSelection = nodeContainsSelectedText(
+      watchedNode.current,
+      selection
+    );
 
-    selectionCb(selection);
-    debouncedSetSelection(selection);
+    if (nodeContainsSelection) {
+      selectionCb(selection);
+      setSelectedText(selection);
+    } else {
+      // clear values when selection is outside the node we're concerned with
+      selectionCb(selection);
+      setSelectedText(null);
+    }
   }
 
   useEffect(() => {
@@ -46,7 +77,12 @@ export default function useTextSelection(debounceMs, selectionCb = noop) {
     return () => {
       document.removeEventListener("selectionchange", handleSelectionChange);
     };
-  }, [selectionCb]);
+  }, [selectionCb, watchedNode.current]);
 
-  return [selectionText, selectionObject.current, selectionRects.current];
+  return [
+    selectionText,
+    selectionObject.current,
+    selectionRects.current,
+    watchedNode
+  ];
 }
