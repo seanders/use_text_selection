4a78e6d2d68732c562853e52975ae87e7ae18b18
Sean Miller <slicebo123@gmail.com>
2019-11-25 16:07:16 -0800

    more stuff

--- index.jsx
+++ index.jsx
@@ -1,6 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import useTextSelection from "./useTextSelection.js";
+import { Popper } from "react-popper";
+
+class VirtualReference {
+  getBoundingClientRect() {
+    return {
+      top: 10,
+      left: 10,
+      bottom: 20,
+      right: 100,
+      width: 90,
+      height: 10
+    };
+  }
+
+  get clientWidth() {
+    return this.getBoundingClientRect().width;
+  }
+
+  get clientHeight() {
+    return this.getBoundingClientRect().height;
+  }
+}
+
+// This is going to create a virtual reference element
+// positioned 10px from top and left of the document
+// 90px wide and 10px high
+const virtualReferenceElement = new VirtualReference();
+
+// This popper will be positioned relatively to the
+// virtual reference element defined above
+const Example = () => (
+  <Popper referenceElement={virtualReferenceElement}>
+    {({ ref, style, placement, arrowProps }) => (
+      <div ref={ref} style={style} data-placement={placement}>
+        Popper element
+        <div ref={arrowProps.ref} style={arrowProps.style} />
+      </div>
+    )}
+  </Popper>
+);
 const debounce = (fn, debounceTime) => {
   // debounced function
   let timeoutId;
